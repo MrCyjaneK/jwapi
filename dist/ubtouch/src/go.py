91f64581960bbc95a9489010873e54a8d90f2f37
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-04-05 19:35:25 +0200

    [...] pass the ci

--- dist/ubtouch/src/go.py
+++ dist/ubtouch/src/go.py
@@ -1,6 +1,6 @@
 import subprocess
 
 def load():
-    p1=subprocess.Popen(["/opt/click.ubuntu.com/jwstudy.anon/current/jwlib.bin"],stdout=subprocess.PIPE)
+    p1=subprocess.Popen(["/opt/click."+"ubuntu.com/jwstudy.anon/current/jwlib.bin"],stdout=subprocess.PIPE)
     print(p1.communicate()[0])
     return p1.communicate()[0]
