917f18fbc2b9a8779718a3088851ea55dc24488c
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-04-06 23:58:09 +0200

    added an option to skip android builds, because it fail on my ci

--- dist/build.sh
+++ dist/build.sh
@@ -46,23 +46,25 @@ echo -n -e "|- bin/jwstudy_ubtouch_arm........"
 GOOS=linux GOARCH=arm     go build --ldflags "-X main.dataDir=/home/phablet/.local/share/jwstudy.anon" -o bin/jwstudy_ubtouch_arm ../../ && ok
 echo -n -e "\_ bin/jwstudy_ubtouch_amd64......"
 GOOS=linux GOARCH=amd64   go build --ldflags "-X main.dataDir=/home/phablet/.local/share/jwstudy.anon" -o bin/jwstudy_ubtouch_amd64 ../../ && ok
-echo "/ android builds - daemon (custom location) + NDK"
-# Android Version
-AV=21
-# NDK downloaded in android studio -> tools? -> sdk manager
-NDKV=$(ls ~/Android/Sdk/ndk/* -d | tr "/" "\n" | tail -1)
-export ANDROID_HOME=~/Android/Sdk
-export ANDROID_NDK_ROOT=~/Android/Sdk/ndk/$NDKV
-NDK=~/Android/Sdk/ndk/$NDKV/toolchains/llvm/prebuilt/linux-x86_64/bin
-echo -n -e "|- bin/jwstudy_android_arm64......"
-CGO_ENABLED=1 CC=$NDK/aarch64-linux-android$AV-clang CXX=$NDK/aarch64-linux-android$AV-clang GOOS=android GOARCH=arm64     go build --ldflags "-X main.dataDir=/data/data/x.x.jwstudy/" -o bin/jwstudy_android_arm64 ../../ && ok
-echo -n -e "|- bin/jwstudy_android_arm........"
-CGO_ENABLED=1 CC=$NDK/armv7a-linux-androideabi$AV-clang CXX=$NDK/armv7a-linux-androideabi$AV-clang GOOS=android GOARCH=arm go build --ldflags "-X main.dataDir=/data/data/x.x.jwstudy/" -o bin/jwstudy_android_arm ../../ && ok
-echo -n -e "|- bin/jwstudy_android_amd64......"
-CGO_ENABLED=1 CC=$NDK/x86_64-linux-android$AV-clang CXX=$NDK/x86_64-linux-android$AV-clang GOOS=android GOARCH=amd64       go build --ldflags "-X main.dataDir=/data/data/x.x.jwstudy/" -o bin/jwstudy_android_amd64 ../../ && ok
-echo -n -e "\_ bin/jwstudy_android_386........"
-CGO_ENABLED=1 CC=$NDK/i686-linux-android$AV-clang CXX=$NDK/i686-linux-android$AV-clang GOOS=android GOARCH=386             go build --ldflags "-X main.dataDir=/data/data/x.x.jwstudy/" -o bin/jwstudy_android_386 ../../ && ok
-
+if [[ "X$SKIPANDROID" == "X" ]];
+then
+    echo "/ android builds - daemon (custom location) + NDK"
+    # Android Version
+    AV=21
+    # NDK downloaded in android studio -> tools? -> sdk manager
+    NDKV=$(ls ~/Android/Sdk/ndk/* -d | tr "/" "\n" | tail -1)
+    export ANDROID_HOME=~/Android/Sdk
+    export ANDROID_NDK_ROOT=~/Android/Sdk/ndk/$NDKV
+    NDK=~/Android/Sdk/ndk/$NDKV/toolchains/llvm/prebuilt/linux-x86_64/bin
+    echo -n -e "|- bin/jwstudy_android_arm64......"
+    CGO_ENABLED=1 CC=$NDK/aarch64-linux-android$AV-clang CXX=$NDK/aarch64-linux-android$AV-clang GOOS=android GOARCH=arm64     go build --ldflags "-X main.dataDir=/data/data/x.x.jwstudy/" -o bin/jwstudy_android_arm64 ../../ && ok
+    echo -n -e "|- bin/jwstudy_android_arm........"
+    CGO_ENABLED=1 CC=$NDK/armv7a-linux-androideabi$AV-clang CXX=$NDK/armv7a-linux-androideabi$AV-clang GOOS=android GOARCH=arm go build --ldflags "-X main.dataDir=/data/data/x.x.jwstudy/" -o bin/jwstudy_android_arm ../../ && ok
+    echo -n -e "|- bin/jwstudy_android_amd64......"
+    CGO_ENABLED=1 CC=$NDK/x86_64-linux-android$AV-clang CXX=$NDK/x86_64-linux-android$AV-clang GOOS=android GOARCH=amd64       go build --ldflags "-X main.dataDir=/data/data/x.x.jwstudy/" -o bin/jwstudy_android_amd64 ../../ && ok
+    echo -n -e "\_ bin/jwstudy_android_386........"
+    CGO_ENABLED=1 CC=$NDK/i686-linux-android$AV-clang CXX=$NDK/i686-linux-android$AV-clang GOOS=android GOARCH=386             go build --ldflags "-X main.dataDir=/data/data/x.x.jwstudy/" -o bin/jwstudy_android_386 ../../ && ok
+fi
 echo "===== Packaging"
 echo "/ Packaging for Ubuntu Touch"
 
@@ -86,42 +88,45 @@ do
     ok
 done
 echo "\_ DONE"
-echo "/ Packaging for android"
-for arch in arm64 arm amd64 386 all
-do
-    echo -n -e "|- bin/jwstudy.android.$arch.apk.........." | head -c 34
-    cd "$builddir"
-    cp ../android android-target-$arch -r
-    cd android-target-$arch
-    touch "$builddir/android-target-$arch/app/src/main/resources/lib/x86_64/jwlib.bin"
-    touch "$builddir/android-target-$arch/app/src/main/resources/lib/x86/jwlib.bin"
-    touch "$builddir/android-target-$arch/app/src/main/resources/lib/armeabi-v7a/jwlib.bin"
-    touch "$builddir/android-target-$arch/app/src/main/resources/lib/arm64-v8a/jwlib.bin"
-    case $arch in
-    "amd64")
-        cp "$builddir/bin/jwstudy_android_amd64" "$builddir/android-target-$arch/app/src/main/resources/lib/x86_64/jwlib.bin"
-        ;;
-    "386")
-        cp "$builddir/bin/jwstudy_android_386"   "$builddir/android-target-$arch/app/src/main/resources/lib/x86/jwlib.bin"
-        ;;
-    "arm")
-        cp "$builddir/bin/jwstudy_android_arm"   "$builddir/android-target-$arch/app/src/main/resources/lib/armeabi-v7a/jwlib.bin"
-        ;;
-    "arm64")
-        cp "$builddir/bin/jwstudy_android_arm64" "$builddir/android-target-$arch/app/src/main/resources/lib/arm64-v8a/jwlib.bin"
-        ;;
-    "all")
-        cp "$builddir/bin/jwstudy_android_amd64" "$builddir/android-target-$arch/app/src/main/resources/lib/x86_64/jwlib.bin"
-        cp "$builddir/bin/jwstudy_android_386"   "$builddir/android-target-$arch/app/src/main/resources/lib/x86/jwlib.bin"
-        cp "$builddir/bin/jwstudy_android_arm"   "$builddir/android-target-$arch/app/src/main/resources/lib/armeabi-v7a/jwlib.bin"
-        cp "$builddir/bin/jwstudy_android_arm64" "$builddir/android-target-$arch/app/src/main/resources/lib/arm64-v8a/jwlib.bin"
-        ;;
-    esac
-    chmod +x $(find . -name jwlib.bin)
-    sed -i 's/BUILD_VERSION_CODE/'$vcode'/g' app/build.gradle
-    ./gradlew build
-    cp ./app/build/outputs/apk/debug/app-debug.apk "$builddir/bin/jwstudy.android.$arch.apk"
-    ok
-done
-echo "\_ OK"
+if [[ "X$SKIPANDROID" == "X" ]];
+then
+    echo "/ Packaging for android"
+    for arch in arm64 arm amd64 386 all
+    do
+        echo -n -e "|- bin/jwstudy.android.$arch.apk.........." | head -c 34
+        cd "$builddir"
+        cp ../android android-target-$arch -r
+        cd android-target-$arch
+        touch "$builddir/android-target-$arch/app/src/main/resources/lib/x86_64/jwlib.bin"
+        touch "$builddir/android-target-$arch/app/src/main/resources/lib/x86/jwlib.bin"
+        touch "$builddir/android-target-$arch/app/src/main/resources/lib/armeabi-v7a/jwlib.bin"
+        touch "$builddir/android-target-$arch/app/src/main/resources/lib/arm64-v8a/jwlib.bin"
+        case $arch in
+        "amd64")
+            cp "$builddir/bin/jwstudy_android_amd64" "$builddir/android-target-$arch/app/src/main/resources/lib/x86_64/jwlib.bin"
+            ;;
+        "386")
+            cp "$builddir/bin/jwstudy_android_386"   "$builddir/android-target-$arch/app/src/main/resources/lib/x86/jwlib.bin"
+            ;;
+        "arm")
+            cp "$builddir/bin/jwstudy_android_arm"   "$builddir/android-target-$arch/app/src/main/resources/lib/armeabi-v7a/jwlib.bin"
+            ;;
+        "arm64")
+            cp "$builddir/bin/jwstudy_android_arm64" "$builddir/android-target-$arch/app/src/main/resources/lib/arm64-v8a/jwlib.bin"
+            ;;
+        "all")
+            cp "$builddir/bin/jwstudy_android_amd64" "$builddir/android-target-$arch/app/src/main/resources/lib/x86_64/jwlib.bin"
+            cp "$builddir/bin/jwstudy_android_386"   "$builddir/android-target-$arch/app/src/main/resources/lib/x86/jwlib.bin"
+            cp "$builddir/bin/jwstudy_android_arm"   "$builddir/android-target-$arch/app/src/main/resources/lib/armeabi-v7a/jwlib.bin"
+            cp "$builddir/bin/jwstudy_android_arm64" "$builddir/android-target-$arch/app/src/main/resources/lib/arm64-v8a/jwlib.bin"
+            ;;
+        esac
+        chmod +x $(find . -name jwlib.bin)
+        sed -i 's/BUILD_VERSION_CODE/'$vcode'/g' app/build.gradle
+        ./gradlew build
+        cp ./app/build/outputs/apk/debug/app-debug.apk "$builddir/bin/jwstudy.android.$arch.apk"
+        ok
+    done
+    echo "\_ OK"
+fi
 echo "DONE! Everything is inside $builddir/bin"
