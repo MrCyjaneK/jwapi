a64d4923dbaa13fdf5033f2e2a33979d23143561
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-04-18 23:40:00 +0200

    edit the way I log things

--- gui/default.go
+++ gui/default.go
@@ -5,7 +5,7 @@ package gui
 import "log"
 
 func Start() {
-	log.Println("Hello! It looks like you forget to specify a correct tag.")
-	log.Println("Please build me with `-tags gui`, to enable gui or with `-tags nogui` to disable it")
-	log.Println("I'll exit now. Bye.")
+	log.Println("[gui] Hello! It looks like you forget to specify a correct tag.")
+	log.Println("[gui] Please build me with `-tags gui`, to enable gui or with `-tags nogui` to disable it")
+	log.Println("[gui] I'll exit now. Bye.")
 }
